398c158efa25ffa26488641d54045ba4cd6c80ef
yangwoo lee <679oose@gmail.com>
2022-12-20 17:15:49 +0900

    2022-12-20

--- data.py
+++ data.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 class book_corpus(Dataset):
     def __init__(self, dir) -> None:
-        self.data = pd.read_csv(dir)["0"].tolist()
+        # data = [token, num, sen]을 가진 Pd.DataFrame
+        self.data = pd.read_csv(dir, index_col=0)["sen"].tolist()
 
     def __getitem__(self, index):
         return self.data[index]
